5ae4d4196908fae01c47c00966baad0d96b5b2a7
Joey <joeyvandervalk@gmail.com>
2024-01-27 17:19:50 +0100

    Binary search

---
+++ Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿namespace DSA;
+
+public static class BinarySearch
+{
+    public static bool BinarySearchIterative(int[] array, int target)
+    {
+        var left = 0;
+        var right = array.Length - 1;
+
+        while (left <= right)
+        {
+            var pivot = (left + right) / 2;
+
+            if (array[pivot] == target)
+                return true;
+            else if (array[pivot] < target)
+                left = pivot + 1;
+            else
+                right = pivot - 1;
+        }
+
+        return false;
+    }
+
+    public static bool BinarySearchRecursive(int[] array, int target, int start, int end)
+    {
+        if (start > end || array.Length == 0) return false;
+
+        var pivot = (start + end) / 2;
+
+        if (array[pivot] == target)
+            return true;
+        else if (array[pivot] > target)
+            return BinarySearchRecursive(array, target, start, pivot - 1);
+        else
+            return BinarySearchRecursive(array, target, pivot + 1, end);
+    }
+}
